39ac2388ba29a6e09d6754fc70e66fb7f6321b50
oskarkk <16339637+oskarkk@users.noreply.github.com>
2020-04-03 04:59:45 +0200

    Add scrapping of test data from Wikipedia

--- corona.py
+++ corona.py
@@ -1,10 +1,13 @@
 import requests
-from flag import flag
+from flag import flag # flag('us')
 #import emoji
 from prettyprinter import cpprint
+from bs4 import BeautifulSoup
 
 
 stats_url = 'https://corona-stats.online/?format=json&source=2'
+tests_url = 'https://en.wikipedia.org/wiki/Template:COVID-19_testing'
+
 resp = requests.get(stats_url).json()
 
 def save(data, filename):
@@ -20,3 +23,34 @@ def world(data):
 
 def pretty(x):
     return cpprint(x, indent=2)
+
+def polska():
+    return
+
+def tests():
+    html = requests.get(tests_url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.select('table.wikitable.sortable')
+    countries = table[0].tbody.find_all('tr')[1:]
+
+    headers = ['country', 'tests', 'positive', 'date',
+        'tests_per_million_people', 'positive_per_thousand_tests']
+
+    data = []
+    for country in countries:
+        cells = country(['th','td'])
+        # nie zadziała bo nie każda komórka ma zawartość
+        #stats = dict(zip( headers, [next(x.stripped_strings) for x in cells] ))
+        values = []
+        for cell in cells:
+            try:
+                values.append(next(cell.stripped_strings))
+            except StopIteration:
+                values.append('')
+                pass
+        stats = dict(zip(headers, values))
+        # nie zadziała bo nie w każdym wierszu jest ten atrybut daty
+        # stats['date'] = cells[3].span['data-sort-value']
+        data.append(stats)
+
+    return data
